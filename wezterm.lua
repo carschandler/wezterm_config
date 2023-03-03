6f61c7837eb90a1bc1dce35a905b7ea397ec1435
Cars Chandler <carschandler7@gmail.com>
2023-03-02 23:12:27 -0600

    Remove font hinting

--- wezterm.lua
+++ wezterm.lua
@@ -3,5 +3,6 @@ local wezterm = require('wezterm')
 return {
    default_prog = {'C:\\WINDOWS\\system32\\wsl.exe', '-d', 'Ubuntu-22.04', '--cd', '~'},
    font = wezterm.font('Source Code Pro', {weight = 'Medium'}),
+   freetype_load_flags = 'NO_HINTING',
    color_scheme = "Gruvbox dark, medium (base16)",
 }
